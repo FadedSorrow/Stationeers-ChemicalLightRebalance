2210b03dee5239f76292e176cd5b92f0011c95ea
FadedSorrow <diminished.sorrow@gmail.com>
2022-07-27 10:48:14 -0500

    First pass files: Work to rebalance the duration, intensity, shadow effects, etc.

---
+++ LongerChemLightPatcher.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+
+namespace StationeersLongChemLight
+{
+    [BepInPlugin("FadedSorrow.Stationeers.Plugins.LongerChemLight", "Longer Chemical Light", "0.1.0.0")]
+    public class LongerChemLightPlugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Log("Plugin for Longer Chemical Light is loaded!");
+
+            try
+            {
+                var harmony = new Harmony("FadedSorrow.Stationeers.Plugins.LongerChemLight");
+                harmony.PatchAll();
+                Log("Patcher completed!");
+            } catch (Exception ex)
+            {
+                Log($"Patcher failed due to: {ex}");
+            }
+        }
+
+        public void Log(string msg)
+        {
+            Logger.LogInfo($"{msg}");
+        }
+    }
+}
---
+++ LongerChemLightPatches.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+using Assets.Scripts.Objects.Items;
+
+namespace StationeersLongChemLight
+{
+    [HarmonyPatch(typeof(ChemLight))]
+    class AlterationPatch
+    {
+        [HarmonyPatch("Start")]
+        [HarmonyPrefix]
+        public static void DurationPatch(ChemLight __instance)
+        {
+            __instance.LifeSpan = 300f; //100f
+        }
+
+        
+
+        [HarmonyPatch("OnInteractableUpdated")]
+        [HarmonyPrefix]
+        public static void RadiancePatch(ChemLight __instance)
+        {
+            Traverse.Create(__instance).Field("_light").Property("intensity").SetValue(2.1f); //1.5f
+            Traverse.Create(__instance).Field("_light").Property("range").SetValue(6.5f); //3f
+            Traverse.Create(__instance).Field("_light").Property("bounceIntensity").SetValue(0.5f); //0f
+            Traverse.Create(__instance).Field("_light").Property("shadowStrength").SetValue(0.5f); //1f
+            Traverse.Create(__instance).Field("_light").Property("shadowSoftness").SetValue(6f); //4f
+            Traverse.Create(__instance).Field("_light").Property("shadowSoftnessFade").SetValue(2f); //1f
+
+            /*var intensity = Traverse.Create(__instance).Field("_light").Property("intensity").GetValue();
+            Log($"View() intensity: {intensity}");
+
+            var range = Traverse.Create(__instance).Field("_light").Property("range").GetValue();
+            Log($"View() range: {range}");*/
+
+
+        }
+
+        
+
+        public static void Log(string msg)
+        {
+            Debug.Log($"StationeersLongChemLight: {msg}");
+        }
+    }
+}
